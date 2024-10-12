1e566a010668177abcd39d0845dd3a7a39050bea
Bekzod <bekzodnosirov02@gmail.com>
2024-10-12 12:56:48 +0500

    add notification send email

--- notification/src/main/java/demo/ecommerce/notification/NotificationRepository.java
+++ notification/src/main/java/demo/ecommerce/notification/NotificationRepository.java
@@ -3,5 +3,5 @@ package demo.ecommerce.notification;
 import demo.ecommerce.kafka.payment.PaymentConfirmation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface NotificationRepository extends MongoRepository<PaymentConfirmation, String> {
+public interface NotificationRepository extends MongoRepository<Notification, String> {
 }
